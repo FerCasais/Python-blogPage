ce0bddec1e07724359411b9bfe39fe711ec1af70
Fernando Casais <fpcasais@gmail.com>
2023-11-16 15:00:17 -0300

    create model Articles

--- AppBlogs/models.py
+++ AppBlogs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Articles(models.Model):
+    title = models.CharField(max_length=256)
+    subTitle = models.CharField(max_length=256)
+    body = models.CharField(max_length=256)
+    author = models.CharField(max_length=20, blank=True, null=True)
+    date = models.DateField(null=True)
+    image = models.CharField(max_length=32)
+    
+
+
+    def __str__(self):
+        return f"{self.title}, {self.subTitle}"
